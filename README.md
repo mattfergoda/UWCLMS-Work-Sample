3c0ec21eb73343fa2d30da6cf967678f9d2ea71b
mattfergoda <48230076+mattfergoda@users.noreply.github.com>
2020-02-14 09:31:06 -0800

    Uploading project

---
+++ README.md
@@ -0,0 +1,39 @@
+
+# UW CLMS Application Work Sample
+## Matt Fergoda
+### Coding project submitted as a work sample for admission to UW CLMS in March 2020.
+
+
+**Applicant**: Matt Fergoda
+**Course**: Harvard’s CS50: Introduction to Computer Science via EdX
+**Dates taken**: May - Sept. 2019
+[**Link to full assignment description**](https://docs.cs50.net/2019/x/psets/4/speller/hashtable/speller.html)
+
+1. To access the project in CS50’s proprietary IDE, go to [this link](https://ide.cs50.io/mattfergoda/ide) (this is likely the simplest way to run the program).
+
+2. Using the IDE’s terminal command line, `cd` into the folder `UW_CLMS_Work_Sample`.
+
+3. To have `clang` compile the program according to the `Makefile`’s specifications, run `make speller`.
+
+4. The program’s usage is `./speller [dictionary] text`
+    * There are two dictionaries available with which to run the program: one “small” (with just two) and one “large” (with several thousand words), both of which can be found in the `dictionaries/`.
+    * If the argument is omitted, the program will default to the large dictionary.
+    * There are a variety of texts with which to run the program, found in the `texts/`.
+    * Answer keys demonstrating the correct program output using the large dictionary can be found in the `keys/`.
+    * For example, the command
+        ```./speller dictionaries/small texts/carroll.txt```
+    will run the program on carroll.txt referencing the small dictionary while
+        ```./speller dictionaries/large texts/holmes.txt```
+    or simply
+        ```./speller texts/holmes.txt```
+    will run the program on holmes.txt referencing the large dictionary.
+    * I suggest using
+   ``` ./speller [dictionary] text > output.txt```
+    to print the output to a `.txt` file instead of the terminal, making it easier to compare the output to the key and keeping the terminal free of output.
+
+5. To compare the output of my program to an answer key one can run, for example:
+   ```
+      ./speller texts/holmes.txt > output.txt
+       diff -y output.txt keys/holmes.txt > diff.txt
+    ```
+    The output file `diff.txt` will print the program’s output for `holmes.txt` and the corresponding key side by side with `<` characters indicating lines that differ. (Note that my program will have five lines of runtime information at the bottom of the output not included in the answer keys).
---
+++ dictionary.c
@@ -0,0 +1,170 @@
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+#include "dictionary.h"
+
+/*
+ Represents number of bins in the hash table for hash function
+ below, a polynomial rolling hash as described here:
+ https://cp-algorithms.com/string/string-hashing.html
+ Source suggests a large prime number for number of bins.
+ This one is from Pietro Cataldi (https://en.wikipedia.org/wiki/Largest_known_prime_number)
+ This number of bins gives a load factor (# entries / # of bins) that is
+ less than 1 (143091 / 524287 = 0.27), which helps ensure the
+ constant time property of the hash table. Though the load factor
+ here is a bit low, indicating wasted memory,the next smallest prime number (131,071)
+ would yeild a load factor greater than 1.
+ In the interest of prioritizing speed over memory usage, I chose 524,287.
+*/
+const int HBINS = 524287;
+
+
+// Initialize a bool to store whether a dictionary has been loaded
+bool loaded = false;
+
+// Initialize an int to to count the number of words in the dictionary,
+// used in size() function.
+unsigned int words = 0;
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+}
+node;
+
+// Instantiate the hash table as an array of nodes
+node *hashtable[HBINS] = { NULL };
+
+
+// A polynomial rolling hash function, as described:
+// https://cp-algorithms.com/string/string-hashing.html
+//
+// I found this generally performs check() in about 0.75s on holmes.txt
+// whereas a simple hash summing the ascii values of a word
+// performs check() in about 3s on holmes.txt.
+
+unsigned int hash(const char *word)
+{
+    unsigned int hash = 0;
+    unsigned int p = 31; // Prime number generally ideal for English alphabet in one case (upper or lower), according to above source.
+    unsigned int p_power = 1; // Powers are hard in C. This will simplify it.
+    for (int i = 0; i < strlen(word) - 1; i++)
+    {
+        hash += tolower(word[i]) * p_power;
+        p_power = p_power * p;
+    }
+    hash = hash % HBINS;
+    return hash;
+}
+
+// Loads dictionary into memory, returning true if successful else false
+bool load(const char *dictionary)
+{
+    // Open dictionary, check that it opens correctly otherwise return false
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        unload();
+        return false;
+    }
+
+    // Buffer for a word
+    char word[LENGTH + 1];
+
+    // Insert words into hash table
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        node *new_node = malloc(sizeof(node));
+
+        // Check that new_node was allocated successfully.
+        if (new_node == NULL)
+        {
+            unload();
+            return false;
+        }
+
+        else
+        {
+            // initialize the temporary node
+            memset(new_node, 0, sizeof(node));
+
+            // Copy the word into the word attribute of the new node.
+            strcpy(new_node -> word, word);
+
+            // Hash the new word.
+            unsigned int hashcode = hash(new_node -> word);
+
+            // Insert the new word into the hash table
+            new_node -> next = hashtable[hashcode];
+            hashtable[hashcode] = new_node;
+
+            // Increment words so we know the number of words
+            // in the dictionary for the size() function
+            words++;
+        }
+    }
+
+    // Close dictionary
+    fclose(file);
+
+    // Load successful. Change loaded to true and
+    // return true
+    loaded = true;
+    return true;
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    if (loaded)
+    {
+        return words;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+// Returns true if word is in dictionary else false
+bool check(const char *word)
+{
+    unsigned int hashword = hash(word); // Hash the word we're checking for
+    node *cursor = hashtable[hashword]; // Start the cursor at the corresponding place in the hash table
+    while (cursor != NULL)
+    {
+        if (strcasecmp(cursor -> word, word) == 0)
+        {
+            return true;
+        }
+        else
+        {
+            cursor = cursor -> next;
+        }
+    }
+    return false;
+}
+
+// Unloads dictionary from memory, returning true if successful else false
+bool unload(void)
+{
+    for (int i = 0; i < HBINS; i++)
+    {
+        node *cursor = hashtable[i];
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor -> next;
+            free(temp);
+        }
+    }
+    return true;
+}
